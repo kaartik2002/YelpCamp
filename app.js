2351be5766b3f58019d5a2adae9d18462edab9e6
Kartikeya Sharma <kartik200902sharma@gmail.com>
2024-09-12 14:47:24 +0530

    dbUrl change

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const reviewRoutes = require('./routes/reviews');
  
 const MongoDBStore = require("connect-mongo")(session);
 mongoose.set('strictQuery', true);
-const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelp-camp';
+const dbUrl = 'mongodb://127.0.0.1:27017/yelp-camp';
 mongoose.connect(dbUrl, {});
 
 
